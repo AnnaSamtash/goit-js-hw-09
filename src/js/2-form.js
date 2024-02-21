a2f8fa32d764f8e2bb9c12491944717d7ae740c9
AnnaSamtash <sansara6900@gmail.com>
2024-02-21 11:35:36 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,23 +1,51 @@
-const form = document.querySelector(".feedback-form");
-const message = form.elements.message;
-const email = form.elements.email;
-const localStorageKey = "feedback-form-state";
+const form = document.querySelector('.feedback-form');
+const messageOfInput = form.elements.message;
+const emailOfInput = form.elements.email;
+const localStorageKey = 'feedback-form-state';
 
-const dataFromLocalStorage = localStorage.getItem(localStorageKey) ?? "";
+const dataFromLocalStorage = localStorage.getItem(localStorageKey) ?? '';
 if (dataFromLocalStorage) {
-  const dataParsed = JSON.parse(dataFromLocalStorage);
-  message.value = dataParsed.message;
-  email.value = dataParsed.email;
+  const { email = '', message = '' } = JSON.parse(dataFromLocalStorage);
+  if (message) {
+    messageOfInput.value = message;
+  }
+  if (email) {
+    emailOfInput.value = email;
+  }
 }
 
-form.addEventListener("input", () => {
-  const dataOfInput = { email: email.value, message: message.value };
-  localStorage.setItem(localStorageKey, JSON.stringify(dataOfInput));
-});
+form.addEventListener('input', saveData);
+form.addEventListener('submit', uploadData);
 
-form.addEventListener("submit", (evt) => {
+function saveData() {
+  if (emailOfInput.value !== "" && messageOfInput.value !== "") {
+    const dataOfInput = {
+      email: emailOfInput.value,
+      message: messageOfInput.value,
+    };
+    const trimedDataOfInput = trimValues(dataOfInput);
+    localStorage.setItem(localStorageKey, JSON.stringify(trimedDataOfInput));
+  }
+}
+
+function trimValues(obj) {
+  for (let key in obj) {
+    obj[key] = obj[key].trim();
+  }
+  return obj;
+}
+
+function uploadData(evt) {
   evt.preventDefault();
-  console.log({ email: evt.target.elements.email.value, message: evt.target.elements.message.value });
+  if (emailOfInput.value === "" || messageOfInput.value === "") {
+    alert("Download all input fields");
+    return;
+  }
+  console.log({
+    email: evt.target.elements.email.value,
+    message: evt.target.elements.message.value,
+  });
   localStorage.removeItem(localStorageKey);
   form.reset();
-});
+}
+
